586fe287932d4da384661e87babe4263ecb7992d
Caio <caiohoborghi@gmail.com>
2021-03-19 15:12:08 -0300

    Adding Mobile

---
+++ MicCheck.Mobile/MicCheck.Mobile/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MicCheck.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        private static string _baseViewUrl = "http://10.0.2.2:13601/";
+        private WebView _webview;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Debug.WriteLine("MainPage.Ctor -> started...");
+
+            _webview = new WebView()
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Source = _baseViewUrl
+            };
+
+            Content = _webview;
+
+            Debug.WriteLine("MainPage.Ctor -> completed");
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            Debug.WriteLine("MainPage.OnBackButtonPressed -> started...");
+            _webview.GoBack();
+            Debug.WriteLine("MainPage.OnBackButtonPressed -> finished");
+            return true;
+        }
+    }
+}
